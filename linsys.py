a6bd90561d45eb887279191572a36caf7d6bfcac
Eduardo Tavares <eduardo.tavares@cortex-intelligence.com>
2016-10-05 15:06:41 -0300

    Added jacobi method

--- linsys.py
+++ linsys.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
+from copy import deepcopy
 
 class LinearSystem:
 
-    def __init__(self, matrix):
+    def __init__(self, matrix, precision):
         self.__matrix = matrix
         self.__normalize_matrix()
+        self.__precision = precision
+
+        self.method = {
+            'gauss': self.gauss,
+            'jacobi': self.jacobi,
+            'gauss_seidel': self.gauss_seidel
+        }
 
     def __normalize_matrix(self):
         m = self.__matrix
@@ -16,13 +24,17 @@ class LinearSystem:
         m = self.__matrix
         for x in xrange(len(m)):
             for y in xrange(len(m[0])):
-                m[x][y] = round(m[x][y], 4)
+                m[x][y] = round(m[x][y], self.__precision)
 
     def show_matrix(self):
+        mcopy = list(self.__matrix)
+
         self.__round_matrix()
         for r in self.__matrix:
             print r
         print ''
+
+        self.__matrix = list(mcopy)
 
     def gauss(self):
         m = self.__matrix
@@ -32,6 +44,7 @@ class LinearSystem:
                 for y in xrange(len(m[0])):
                     if x > piv and y > piv:
                         m[x][y] = m[x][y] - (m[x][piv]/m[piv][piv]) * m[piv][y]
+            self.show_matrix()
 
         for x in xrange(len(m)-1, -1, -1):
             for y in xrange(len(m[0])-1, -1, -1):
@@ -46,3 +59,43 @@ class LinearSystem:
                         m[x][y] = 0
                     elif x > y:
                         m[x][y] = 0
+            self.show_matrix()
+
+    def jacobi(self):
+        m = self.__matrix
+        r = [0 for x in xrange(len(m))]
+
+        for x in xrange(len(m)):
+            piv = m[x][x]
+            for y in xrange(len(m[0])):
+                if x != y:
+                    m[x][y] = m[x][y]/piv
+            m[x][x] = 1
+
+        mc = None
+        rc = None
+        counter = 0
+
+        while r != rc:
+            print counter,'=',r
+            mc = deepcopy(m)
+            rc = deepcopy(r)
+
+            for x in xrange(len(m)):
+                for y in xrange(len(m[0])):
+                    if x != y and y < len(r):
+                        mc[x][y] = mc[x][y] * r[y]
+                        mc[x][-1] = mc[x][-1] - mc[x][y]
+                        mc[x][y] = 0
+
+            for x in xrange(len(m)):
+                r[x] = mc[x][-1]
+
+            counter = counter + 1
+
+        self.__matrix = deepcopy(mc)
+        print ''
+        self.show_matrix()
+
+    def gauss_seidel(self):
+        m = self.__matrix
--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
 from csvmanager import CsvManager
 from linsys import LinearSystem
+import sys
 
-def main():
-    # method =
-    # input_name =
-    # output_name =
+def main(method):
+    csvm = CsvManager('input.csv')
+    linear = LinearSystem(csvm.to_matrix(), 4)
 
-    csvm = CsvManager('10.csv')
-    linear = LinearSystem(csvm.to_matrix())
-    linear.show_matrix()
-    linear.gauss()
-    linear.show_matrix()
-
-def help():
-    print ''
+    try:
+        run_method = linear.method[method]
+        linear.show_matrix()
+        run_method()
+    except KeyError:
+        print 'Algoritmo inexistente. Tente essas opcoes:'
+        for key in linear.method:
+            print key
 
 if __name__ == '__main__':
-    main()
+    try:
+        main(sys.argv[1])
+    except IndexError:
+        print 'Execute o programa novamente informando algoritmo desejado como parametro.'
